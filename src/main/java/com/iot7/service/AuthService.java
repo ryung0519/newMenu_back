9ac6de8986e304a3c41b59be2f4254c6b39cf52b
haram-jo <harramaram@gmail.com>
2025-03-24 16:12:49 +0900

    회원가입 비번 누락 추가!

--- src/main/java/com/iot7/service/AuthService.java
+++ src/main/java/com/iot7/service/AuthService.java
@@ -1,4 +1,53 @@
 package com.iot7.service;
 
+import com.iot7.dto.UserSignupRequest;
+import com.iot7.entity.User;
+import com.iot7.repository.UserRepository;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // 🔹 회원가입하기~
+    public User registerUser(UserSignupRequest request) throws Exception {
+        String uid = request.getUid();
+        // 이메일 중복 체크
+        Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
+        if (existingUser.isPresent()) {
+            throw new Exception("이미 가입된 이메일입니다.");
+        }
+        // 유저 저장
+        User newUser = new User();
+        newUser.setUserId(uid); //아이디 주입
+        newUser.setUserName(request.getName()); //이름 주입
+        newUser.setEmail(request.getEmail()); //이메일 주입
+        newUser.setPassword(request.getPassword()); // 비번 주입
+        newUser.setPreferredFood(request.getPreferredFood());       //좋아하는 음식 주입
+        newUser.setAllergicFood(request.getAllergicFood());      //싫어하는 음식 주입
+        newUser.setRegDate(new Date()); // 가입날짜
+
+        return userRepository.save(newUser); // 저장받기
+    }
+
+    // 🔹 로그인 하기~
+    public User authenticateUser(String token) throws Exception {
+        // Firebase ID 토큰 검증 > 바꿔여함!!
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+        String email = decodedToken.getEmail();
+
+        // DB에서 사용자 조회
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new Exception("유저를 찾을 수 없습니다."));
+    }
 }
