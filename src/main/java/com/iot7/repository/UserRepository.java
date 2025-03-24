43e2c962598b85b22e64dcd6fdc626e67dd9ab9b
haram-jo <harramaram@gmail.com>
2025-03-24 20:38:36 +0900

    findByEmail 만듦!

--- src/main/java/com/iot7/repository/UserRepository.java
+++ src/main/java/com/iot7/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.iot7.repository;
 
-public class UserRepository {
+import com.iot7.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
