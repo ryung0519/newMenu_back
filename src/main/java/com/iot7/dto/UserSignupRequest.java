da6025cb0cd64f5486276110b7f9f27f294fd444
haram-jo <harramaram@gmail.com>
2025-03-24 16:10:26 +0900

    회원가입 password 누락 추가!

--- src/main/java/com/iot7/dto/UserSignupRequest.java
+++ src/main/java/com/iot7/dto/UserSignupRequest.java
@@ -1,4 +1,15 @@
 package com.iot7.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserSignupRequest {
+    private String uid; // Firebase ID Token
+    private String name;
+    private String email;
+    private String password;
+    private String preferredFood;
+    private String allergicFood;
 }
