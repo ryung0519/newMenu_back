03bea44ac669dcdd7f23900ddfdf044169113d96
haram-jo <harramaram@gmail.com>
2025-04-03 15:02:51 +0900

    브랜드 필터링 완료

--- src/main/java/com/iot7/service/MenuService.java
+++ src/main/java/com/iot7/service/MenuService.java
@@ -2,19 +2,21 @@ package com.iot7.service;
 
 import com.iot7.dto.MenuDTO;
 import com.iot7.entity.Menu;
+import com.iot7.repository.BusinessUserRepository;
 import com.iot7.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class MenuService {
 
+    private final BusinessUserRepository businessUserRepository;
     private final MenuRepository menuRepository;
 
-    public MenuService(MenuRepository menuRepository) {
-        this.menuRepository = menuRepository;
-    }
 
     // 🔹 홈에서 카테고리 목록 가져오기
     public List<String> getCategories() {
@@ -30,8 +32,25 @@ public class MenuService {
         return menus;
     }
 
-    // 🔹✅  홈에서 키워드로 메뉴 검색 (이름 또는 재료)
+    // ✅  홈에서 키워드로 메뉴 검색 (이름 또는 재료)
     public List<Menu> searchMenus(String keyword) {
         return menuRepository.findByMenuNameContainingIgnoreCaseOrIngredientsContainingIgnoreCase(keyword, keyword);
     }
+
+
+    // ✅"메가커피 주세요!"  → "메가커피 본점 번호 찾기"
+    // ✅"그 번호로 메뉴 가져오기" → "메뉴  프론트에 전달"
+    public List<MenuDTO> getMenusByBrandMainBranch(String brandName) {
+        Long businessId = businessUserRepository.findMainBusinessIdByName(brandName)
+                .orElseThrow(() -> new RuntimeException("본점 정보 없음"));
+
+        return menuRepository.findByBusinessUser_BusinessId(businessId)
+                .stream()
+                .map(MenuDTO::fromEntity)
+                .collect(Collectors.toList());
+    }
 }
+
+
+
+
