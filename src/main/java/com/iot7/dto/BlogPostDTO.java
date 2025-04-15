6dd738a622eada6ccff7ca131c0618cf4533b411
haram-jo <harramaram@gmail.com>
2025-04-15 19:51:31 +0900

    네이버 API로 블로그 게시물 불러오기

---
+++ src/main/java/com/iot7/dto/BlogPostDTO.java
@@ -0,0 +1,11 @@
+package com.iot7.dto;
+
+import lombok.Data;
+
+@Data
+public class BlogPostDTO {
+    private String title;
+    private String link;
+    private String bloggerName;
+    private String postDate;
+}
