a6bb5c913270f987f7523808ab08c2e601b04b2c
ryung0519 <happy79891@gmail.com>
2025-04-17 15:46:12 +0900

    브ëBrandMenuListScreen ㅜNullPointException오류 해결

--- src/main/java/com/iot7/dto/MenuDTO.java
+++ src/main/java/com/iot7/dto/MenuDTO.java
@@ -30,21 +30,28 @@ public class MenuDTO {
         this.imageUrl = imageUrl; // ✅ 이미지 필드 연결
         this.description = description;
     }
-    public MenuDTO(Long menuId, String menuName, String category, int price, String businessName, String image) {
-    }
-
 
     // ⭐ 필터링 위해서 필요한 엔티티 →  DTO로 변환
     public static MenuDTO fromEntity(Menu menu) {
+        if (menu == null) return null;
+
+        String businessName = menu.getBusinessUser() != null
+                ? menu.getBusinessUser().getBusinessName()
+                : "알 수 없음";
+
         MenuDTO dto = new MenuDTO(
                 menu.getMenuId(),
                 menu.getMenuName(),
                 menu.getCategory(),
                 menu.getPrice(),
-                menu.getBusinessUser().getBusinessName(),
-                menu.getImage()
+                businessName,
+                menu.getImage(),
+                menu.getDescription()
+
         );
 
+        dto.setBrand(businessName); // 브랜드 이름으로도 동일하게 설정
+//        dto.setBrand(menu.getBusinessUser().getBusinessName());
         dto.setImageUrl(menu.getImage()); // ✅ 상세페이지에서 브랜드 눌렀을때 이미지 받아오기 위해 추가
         return dto;
     }
