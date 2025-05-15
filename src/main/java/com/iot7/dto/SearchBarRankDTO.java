47d3147b1eb37a4994ed5d78a0196fc957f451e7
haram-jo <harramaram@gmail.com>
2025-05-15 18:22:46 +0900

    feature: 검색창 눌렀을시 급상승 검색어 기능 ì 중간 커밋

---
+++ src/main/java/com/iot7/dto/SearchBarRankDTO.java
@@ -0,0 +1,15 @@
+// 📁 dto/SearchBarRankDTO.java
+package com.iot7.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchBarRankDTO {
+    private String menuName; // 랭크 보여주는데는 이름만 필요하니까
+}
