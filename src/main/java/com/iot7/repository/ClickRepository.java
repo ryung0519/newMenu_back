afc687c8cd0496d43184c8d9888071495710da5c
haram-jo <harramaram@gmail.com>
2025-05-15 14:55:03 +0900

    refactor: 인기 상품 조회 click_count에서  menu_click_log 방방법으로 변경

--- src/main/java/com/iot7/repository/ClickRepository.java
+++ src/main/java/com/iot7/repository/ClickRepository.java
@@ -6,12 +6,26 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ClickRepository extends JpaRepository<Menu, Long> {
 
-    @Query("SELECT m FROM Menu m WHERE m.businessUser.businessName = :brandName ORDER BY COALESCE(m.clickCount, 0) DESC")
-    List<Menu> findPopularMenusByBrand(@Param("brandName") String brandName);
+
+    //브랜드의 인기상품 조회 click_count가 아니라 menu_click_log로 조회
+    //2주간 클릭수가 가장 많은 제품을 인기상품으로 보여줌
+    @Query("SELECT m " +
+            "FROM Menu m " +
+            "JOIN (SELECT l.menuId as menuId, COUNT(l) as clickCount " +
+            "      FROM MenuClickLog l " +
+            "      WHERE l.clickTime >= :startDate " +  // 2주전부터 오늘까지 날짜 계산
+            "      GROUP BY l.menuId) log " +
+            "ON m.menuId = log.menuId " +
+            "WHERE m.businessUser.businessName = :brandName " +
+            "ORDER BY log.clickCount DESC")
+    List<Menu> findPopularMenusByBrand(@Param("brandName") String brandName,
+                                       @Param("startDate") LocalDateTime startDate);
 }
+
 
--- src/main/java/com/iot7/service/ClickService.java
+++ src/main/java/com/iot7/service/ClickService.java
@@ -4,9 +4,10 @@ import com.iot7.dto.MenuDTO;
 import com.iot7.entity.Menu;
 import com.iot7.repository.ClickRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor; // ✅ 이거 반드시 추가!
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
@@ -16,7 +17,8 @@ public class ClickService {
     private final ClickRepository clickRepository;
 
     public List<MenuDTO> getPopularMenusByBrand(String brandName) {
-        List<Menu> menus = clickRepository.findPopularMenusByBrand(brandName);
+        LocalDateTime twoWeeksAgo = LocalDateTime.now().minusDays(14); //2주전부터 오늘까지 날짜 계산
+        List<Menu> menus = clickRepository.findPopularMenusByBrand(brandName, twoWeeksAgo);
         return menus.stream()
                 .map(MenuDTO::fromEntity)
                 .collect(Collectors.toList());
